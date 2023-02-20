0733cb732539ee2b3f8c3b348401c237ddf78b6b
Yuriy Mironenko <ydtalel@gmail.com>
2023-02-20 23:16:40 +0500

    class added

--- DBManager/DBManager.py
+++ DBManager/DBManager.py
@@ -1,4 +1,3 @@
-
 import sqlalchemy as sa
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
@@ -9,7 +8,8 @@ from DBManager.TableClasses.Favorites import Favorite
 
 class DBManager:
 
-    def __init__(self, db_name :str, db_protocol : str = "postgresql", user_name : str = "postgres", user_password : str = "111", host : str = "localhost", port : str = "5432") -> None:
+    def __init__(self, db_name: str, db_protocol: str = "postgresql", user_name: str = "postgres",
+                 user_password: str = "111", host: str = "localhost", port: str = "5432") -> None:
         self.__DSN = F"{db_protocol}://{user_name}:{user_password}@{host}:{port}/{db_name}"
         self.__engine = sa.create_engine(self.__DSN)
         DeclarativeBase.Base.metadata.create_all(self.__engine)
@@ -19,26 +19,25 @@ class DBManager:
         self.__session.add(User(user_vk_id=vk_id, name=name, age=age, gender=gender))
         self.__session.commit()
 
-    def AddUserFavorites(self, user_id : int, fav_id : int):
+    def AddUserFavorites(self, user_id: int, fav_id: int):
         self.__session.add(Favorite(user_id=user_id, user_fav_id=fav_id))
         self.__session.commit()
 
-    def GetUserByID(self, user_id : int) -> dict:
+    def GetUserByID(self, user_id: int) -> dict:
         x_ret = self.__session.query(User).where(User.user_id == user_id)
         for x in x_ret.all():
-            return {"name" : x.name, "age" : x.age, "gender" : x.gender}
+            return {"name": x.name, "age": x.age, "gender": x.gender}
         return {}
 
     def GetUserByVkID(self, vk_id: str) -> dict:
         x_ret = self.__session.query(User).where(User.user_vk_id == vk_id)
         for x in x_ret.all():
-            return {"user_id" : x.user_id, "name" : x.name, "age" : x.age, "gender" : x.gender}
+            return {"user_id": x.user_id, "name": x.name, "age": x.age, "gender": x.gender}
         return {}
-    
-    def GetUserFavorites(self, user_id : int) -> list:
+
+    def GetUserFavorites(self, user_id: int) -> list:
         x_ret = self.__session.query(Favorite).where(Favorite.user_id == user_id)
         ret_list = list()
         for x in x_ret.all():
             ret_list.append(x.user_fav_id)
         return ret_list
-
--- search/search.py
+++ search/search.py
@@ -1,75 +1,76 @@
 import vk
 from dotenv import load_dotenv
 import os
-from pprint import pprint
-import time
+import random
 
 load_dotenv()  # take environment variables from .env.
 token = os.getenv('token')
-api = vk.API(access_token=token, v=5.131)
 
 
-def get_user_info(id_):
-    user_info = api.users.get(user_ids=id_, fields='id, first_name, last_name, bdate, city, sex')
-    age = 2023 - int(user_info[0]['bdate'].split('.')[2])  # тут потом доделать что бы считал через date
-    sex = user_info[0]['sex']
-    city_id = user_info[0]['city']['id']
-    if sex == 1:
-        opposite_sex = 2
-    else:
-        opposite_sex = 1
-    params = {
-        'age_from': age,
-        'age_to': age + 5,
-        'sex': opposite_sex,
-        'city': city_id
-    }
-    return params
+class Vkinder:
+    def __init__(self, api):
+        self.api = api
+
+    def get_user_info(self, id_):
+        user_info = self.api.users.get(user_ids=id_, fields='id, first_name, last_name, bdate, city, sex')
+        age = 2023 - int(user_info[0]['bdate'].split('.')[2])  # тут потом доделать что бы считал через date
+        sex = user_info[0]['sex']
+        city_id = user_info[0]['city']['id']
+        fname = user_info[0]['first_name']
+        lname = user_info[0]['last_name']
+        return {
+            'vk_id': id_,
+            'name': f"{fname} {lname}",
+            'age': age,
+            'sex': sex,
+            'city': city_id
+        }
+
+    def users_search(self, params):
+        # sort -0 популярность //  sort-1 новые // count максимум 1000
+        if params['sex'] == 1:
+            params['sex'] = 2
+        else:
+            params['sex'] = 1
+        del params['vk_id']
+        del params['name']
+        users_age = params.pop('age')
+        params['age_from'] = users_age - 3
+        params['age_to'] = users_age + 3
+
+        users_search = self.api.users.search(**params, count=1000, is_closed=0, sort=0, has_photo=1, status=6)
+        return [user['id'] for user in users_search['items'] if not user['is_closed']]
+
+    @staticmethod
+    def _find_largest_photo(dict_sizes):
+        if dict_sizes["width"] >= dict_sizes["height"]:
+            return dict_sizes["width"]
+        else:
+            return dict_sizes["height"]
+
+    def _get_all_user_photo(self, id_):
+        all_photos = self.api.photos.getAll(owner_id=id_, extended=True)
+        dict_ = {}
+        for photo in all_photos['items']:
+            sizes = photo['sizes']
+            largest = max(sizes, key=self._find_largest_photo)
+            dict_[largest['url']] = str(photo['likes']['count'])
+        sorted_tuples = sorted(dict_.items(), key=lambda item: item[1])[-3:]
+        return {k: v for k, v in sorted_tuples}.keys()
+
+    def get_candidate(self, list_of_users_id):
+        rand_user = random.choice(list_of_users_id)
+        user_info = self.api.users.get(user_ids=rand_user, fields='id, first_name, last_name')
+        hrefs = self._get_all_user_photo(rand_user)
+        return {
+            'name': f"{user_info[0]['first_name']} {user_info[0]['last_name']}",
+            'link': f"https://vk.com/id{str(user_info[0]['id'])}",
+            'photo': hrefs
+        }
 
 
-def users_search():
-    # sort -0 популярность //  sort-1 новые count максимум 1000
-    users_search = api.users.search(**params, count=5, is_closed=0, sort=0, has_photo=1, status=6)
-    return [user['id'] for user in users_search['items'] if not user['is_closed']]
+vkinder = Vkinder(vk.API(access_token=token, v=5.131))
 
-
-def find_largest_photo(dict_sizes):
-    if dict_sizes["width"] >= dict_sizes["height"]:
-        return dict_sizes["width"]
-    else:
-        return dict_sizes["height"]
-
-
-def get_all_user_photo(id_):
-    all_photos = api.photos.getAll(owner_id=id_, extended=True)
-    dict_ = {}
-    for photo in all_photos['items']:
-        sizes = photo['sizes']
-        largest = max(sizes, key=find_largest_photo)
-        dict_[largest['url']] = str(photo['likes']['count'])
-    sorted_tuples = sorted(dict_.items(), key=lambda item: item[1])[-3:]
-    return {k: v for k, v in sorted_tuples}.keys()
-
-
-def get_data_opposite_sex(list_of_users_id):
-    for user in list_of_users_id:
-        user_info = api.users.get(user_ids=user, fields='id, first_name, last_name')
-        hrefs = get_all_user_photo(user)
-        print(f"{user_info[0]['first_name']} {user_info[0]['last_name']} ссылка на профиль"
-              f" https://vk.com/id{str(user_info[0]['id'])}\n{hrefs}")
-        time.sleep(4)
-
-
-# для парня находим дувушек из того же города примерного возраста со статусом в поиске, с открытым профилем и фото
-params = get_user_info(922473)
-get_data_opposite_sex(users_search())
-
-
-
-
-
-# database_getCities = api.database.getCities(country_id=1, need_all=1)
-# pprint(database_getCities)
-
-# database_getCountries = api.database.getCountries(code='RU')
-# pprint(database_getCountries)
+# group_member = vkinder.get_user_info(922473)
+# candidate_list = vkinder.users_search(group_member)
+# print(vkinder.get_candidate(candidate_list))
