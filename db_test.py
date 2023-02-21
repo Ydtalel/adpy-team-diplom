ab2a69f9fb1f3eb54f321750771f3a45b95aefe8
archimage-wiz <archimage@yandex.ru>
2023-02-21 12:25:49 +0300

    227

--- db_test.py
+++ db_test.py
@@ -1,20 +1,13 @@
 
-
+from random import randrange
 from DBManager.DBManager import DBManager
 
 
 if __name__ == "__main__":
 
     db = DBManager("vkbot_db")
-    db.AddUser("ee3", "vasya", age=23, gender=1)
     x = db.GetUserByVkID("ee3")
     print(x)
-    #db.AddUserFavorites(1, 3)
-    x = db.GetUserFavorites(2)
-    print(x)
-    
-# git ?
-=======
     x = db.AddUser("eezz" + str(randrange(100)), "vasya", age=23, gender=1, city=1)
     print("AddUser", x)
     x = db.AddUserFavorites(1, 3)
@@ -25,6 +18,5 @@ if __name__ == "__main__":
     print("GetUserByVkID", x)
     x = db.GetUserFavorites(1)
     print("GetUserFavorites", x)
->>>>>>> db_correct_dev_branch_test
 
-# git test 225
+# git test 226
