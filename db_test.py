27b0153bbcf53a2b0c8e39f44b14efd0e7d29fd0
archimage-wiz <archimage@yandex.ru>
2023-02-21 12:09:17 +0300

    try to fix it

--- db_test.py
+++ db_test.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 from random import randrange
-=======
->>>>>>> ae9c5b8433a47dd17d85838c5f6c5ec1a5509a7c
 from DBManager.DBManager import DBManager
 
 if __name__ == "__main__":
     db = DBManager("vkbot_db")
-<<<<<<< HEAD
     x = db.AddUser("eezz" + str(randrange(100)), "vasya", age=23, gender=1, city=1)
     print("AddUser", x)
     x = db.AddUserFavorites(1, 3)
